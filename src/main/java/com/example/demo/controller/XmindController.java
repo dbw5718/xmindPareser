829d26456f35193d66aff23580e6871a07a91f6c
dbw5718 <dbw5718@126.com>
2024-10-18 18:53:54 +0800

    fix

--- src/main/java/com/example/demo/controller/XmindController.java
+++ src/main/java/com/example/demo/controller/XmindController.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.Topic;
 import com.example.demo.service.XmindService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,7 +24,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/xmind")
 public class XmindController {
-    @Autowired
+    @Resource
     private XmindService xmindService;
 
     @PostMapping("/upload")
